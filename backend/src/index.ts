4420257c01a07d44b797250044b5bf3fb960bc7b
Dmitriy A Sokolov <dsokolov@cloudfactory.ru>
2026-08-30 09:38:51 +0300

    Logger - bigfix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,9 @@
+import "dotenv/config";
 import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { logger } from "./lib/logger";
 import pinoHttp from "pino-http";
-
-dotenv.config();
 
 const PORT = process.env.PORT || 8000
 
@@ -30,5 +28,5 @@ app.get("/health", (_req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-})
+    logger.info({ port: PORT }, "server started");
+});
